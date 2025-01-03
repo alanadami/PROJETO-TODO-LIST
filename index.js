0687fa5118243ba9f6da508e3e166cff6e86d33b
alanadami <166352617+alanadami@users.noreply.github.com>
2025-01-03 12:16:30 -0300

    Mock e rotas
    Criação de Mock com dados já informados para testes.
    Criação de rota para exibir dados do mock

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const app = require("./conect")
 app.get("/", (req, res) => {
     res.send("Rodando get inicial")
 })
+
+app.get("/tasks", (req, res) => {
+    res.sendFile(__dirname, "task.json")
+})
