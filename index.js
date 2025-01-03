21a581b5159c19876ac764fa6958edac73389ff9
alanadami <166352617+alanadami@users.noreply.github.com>
2025-01-03 20:13:27 -0300

    arquivo Json
    Acessando dados json
    adicionando verificação de status

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+const { log } = require("console")
 const app = require("./conect")
+const path = require("path")
+
 
 app.get("/", (req, res) => {
     res.send("Rodando get inicial")
 })
 
 app.get("/tasks", (req, res) => {
-    res.sendFile(__dirname, "task.json")
+    res.sendFile(path.join(__dirname, "JSON", "tasks.json"), (err) => {
+        if(err){
+            res.status(500).send("Não foi possível localizar o arquivo")
+        }
+    })
+})
+
+app.post("/tasks", (req, res) => {
+    const newTask = {
+        "id": tasks.length + 1
+    }
 })
